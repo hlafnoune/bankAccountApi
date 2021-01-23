b53563cd76ad544616bd689c23b8e26940f3535e
LAFNOUNE Hicham <lafnoune.hicham@gmail.com>
2021-01-23 15:54:08 +0100

    project initialization

---
+++ src/main/java/com/sg/bankaccount/api/BankAccountApiApplication.java
@@ -0,0 +1,13 @@
+package com.sg.bankaccount.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountApiApplication.class, args);
+	}
+
+}
